52653077bd36b98137443934ff3b6c0a56f4cb8a
esantos <eudesgfilho@outlook.com>
2024-02-21 14:07:17 +0000

    Added Deployment

--- setup.sh
+++ setup.sh
@@ -11,6 +11,7 @@ echo \
 sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt update
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo usermod -aG docker $USER && newgrp docker
  
 #minikube setup
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
@@ -19,13 +20,18 @@ sudo install minikube-linux-amd64 /usr/local/bin/minikube
 #kubectl setup
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
 chmod +x kubectl
-sudo mv kubectl /usr/local/bin/s
+sudo mv kubectl /usr/local/bin/
  
 #git setup 
 git clone https://github.com/nodejs/examples
-
-#build docker
-sudo docker build -t eudesgfilho/faurecia-app .
-
+ 
 #starting minikube
 minikube start
+ 
+#building image
+minikube image build -t faurecia-app -f ./Dockerfile .
+ 
+#deploying app
+kubectl apply -f Deployment.yaml
+kubectl wait --for=condition=available faurecia-app --timeout=5m
+kubectl expose deployment faurecia-app --type=NodePort --port=3000
