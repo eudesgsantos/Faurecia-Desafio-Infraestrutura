d6c7214b6adfb581a6ec0121f8b5e36dc2e96c7e
esantos <eudesgfilho@outlook.com>
2024-02-22 05:34:01 +0000

    Corrected typos

--- update.sh
+++ update.sh
@@ -5,12 +5,12 @@ customTag=$1
 
 #update repo
 cd examples
-git fetch -all
+git fetch --all
 git reset --hard origin/main
 
-cd..
+cd ..
 
 #build image
 minikube image build -t faurecia-app:$customTag -f ./Dockerfile .
 
-kubectl set image deployment/faurecia-app faurecia-app=faurecia-app:$customTags
+kubectl set image deployment/faurecia-app faurecia-app=faurecia-app:$customTag
